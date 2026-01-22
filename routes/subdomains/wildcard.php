b8fa0a0eca329f81a6d928983b41b42bc7403262
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 20:59:39 +0100

    Revert "Route Fix"
    This reverts commit f81a903ebd4bcc46d0b88a3c23d4968a97f9f34c.

--- routes/subdomains/wildcard.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::domain('{subdomain}.web.ap.it')->group(function () {
-    Route::get('/{any?}', function () {
-        return response()->view('wildcard', [], 404);
-    })->where('any', '.*');
-});
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Wildcard catchall for unregistered subdomains
+Route::domain('{subdomain}.web.ap.it')->group(function () {
+    Route::get('/{any?}', function () {
+        return response()->view('wildcard', [], 404);
+    })
+        ->where('subdomain', function ($query) {
+            $query->whereNotIn('subdomain', config('subdomains.list'));
+        })
+        ->where('any', '.*');
+});
+
 // Home page
 Route::get('/', function () {
     return view('welcome');
