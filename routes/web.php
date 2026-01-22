0e44faa88a3bcaa8a50a2ce8f8baae5d15d51206
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 19:42:08 +0100

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Wildcard catchall for unregistered subdomains
 Route::domain('{subdomain}.web.ap.it')->group(function () {
     Route::get('/{any?}', function () {
         return response()->view('wildcard', [], 404);
     })->where('any', '.*');
+});
+
+// Home page
+Route::get('/', function () {
+    return view('welcome');
 });
