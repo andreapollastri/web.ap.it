c66db3a3eeef1027a58f214501984ecd80317179
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 20:59:08 +0100

    Revert "Fix"
    This reverts commit bf14169d5aadab4042c69ef932523b15453628de.

--- routes/subdomains/wildcard.php
+++ routes/subdomains/wildcard.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::domain('{subdomain}.web.ap.it')
-    ->where(['subdomain' => '^(?!init$).*'])
+    ->where('subdomain', '^(?!init$).*')
     ->group(function () {
         Route::get('/{any?}', function () {
             return response()->view('wildcard', [], 404);
