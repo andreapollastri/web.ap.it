4acdcb38f6444e58328009d02777da2330303e03
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 11:01:06 +0100

    Response 404 on Wildcard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('{subdomain}.web.ap.it')->group(function () {
     Route::get('/{any?}', function () {
-        return view('wildcard');
+        return response()->view('wildcard', [], 404);
     })->where('any', '.*');
 });
 
