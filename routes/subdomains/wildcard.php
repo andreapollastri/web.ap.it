def5142dec452ec6d3afe31a1e3d85006b989e2f
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 20:59:13 +0100

    Revert "Route fix"
    This reverts commit 568525e1ae11ce44f3767bab0d8b0b249326f42b.

--- routes/subdomains/wildcard.php
+++ routes/subdomains/wildcard.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain('{subdomain}.web.ap.it')
-    ->where('subdomain', '^(?!init$).*')
-    ->group(function () {
-        Route::get('/{any?}', function () {
-            return response()->view('wildcard', [], 404);
-        })->where('any', '.*');
-    });
+Route::domain('{subdomain}.web.ap.it')->group(function () {
+    Route::get('/{any?}', function () {
+        return response()->view('wildcard', [], 404);
+    })->where('any', '.*');
+});
