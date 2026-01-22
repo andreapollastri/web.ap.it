fed8557cddcd78834d9346dd95039bb49833ea89
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 10:58:02 +0100

    Temp pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::domain('{subdomain}.web.ap.it')->group(function () {
+    Route::get('/{any?}', function () {
+        return view('wildcard');
+    })->where('any', '.*');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
