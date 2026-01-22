fda48804ac55ea2bbf70cffc801a1d8648cca561
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2026-01-22 20:59:44 +0100

    Revert "Fix"
    This reverts commit 8e4d81b50bbaea7740cff032c38100fcdc23b1a0.

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::domain('{subdomain}.web.ap.it')->group(function () {
     Route::get('/{any?}', function () {
         return response()->view('wildcard', [], 404);
     })
-        ->where('subdomain', function ($query) {
-            $query->whereNotIn('subdomain', config('subdomains.list'));
-        })
+        ->whereNotIn('subdomain', config('subdomains.list'))
         ->where('any', '.*');
 });
 
